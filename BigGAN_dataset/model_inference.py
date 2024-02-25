f1b26c0735ac6494f035e1dfbce493eb2f40e770
Ahmad1732002 <af3954@nyu.edu>
2024-02-25 23:13:57 +0400

    updated model inference

--- BigGAN_dataset/model_inference.py
+++ BigGAN_dataset/model_inference.py
@@ -1,32 +1,43 @@
-from transformers import BlipForConditionalGeneration
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
 import pandas as pd
 import torch
 import subprocess
-from PIL import Image
-from transformers import AutoProcessor, BlipForConditionalGeneration
-from torchvision.transforms import functional as F
-
+from transformers import AutoProcessor
+# Load the fine-tuned model and processor
 model = BlipForConditionalGeneration.from_pretrained("fine_tuned_model")
-# Specify the device
-processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Set device (GPU if available, otherwise CPU)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
+def inference_for_image(model, processor, image_path, device):
+    # Open and convert the image
+    image = Image.open(image_path).convert('RGB')
+
+    # Process the image
+    #pixel_values = processor(images=image, return_tensors="pt").pixel_values.to(device)
+
+    prompt = "XYZ "
+
+    inputs = processor(image, prompt, return_tensors="pt").to(device)
+
+    out = model.generate(**inputs, max_length=100)
+    generated_text = processor.decode(out[0], special_tokens=True)
+    return generated_text
+
+    # # Generate caption
+    # outputs = model.generate(pixel_values=pixel_values)
+    # caption = processor.batch_decode(outputs, skip_special_tokens=True)[0]  # Assuming one image is processed at a time
+
+    # return caption
+
+# Example usage:
 testing_dataset=pd.read_csv('validated_test_data_csv')
 
 # Assuming `image` is already loaded and processed
-img_path = testing_dataset.iloc[3]['image']
-image = Image.open(img_path).convert('RGB')
-image = F.resize(image, [224, 224])  # Resize image to the required input size
+image_path = testing_dataset.iloc[3]['image']
 
-prompt = "Question: Is this image natural or AI generated and why? Answer:"
-
-inputs = processor(image, text=prompt, return_tensors="pt", padding=True, truncation=True).to(device)
-
-# Generate text
-generated_ids = model.generate(**inputs, max_length=20)
-generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
-print(generated_text)
-#
+caption = inference_for_image(model, processor, image_path, device)
+print("Gen", caption)
